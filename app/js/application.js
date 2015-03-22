33525a5ca483b243148ca0e4127079965606c046
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-03-22 15:52:04 +0000

    layout changed and comments removed

--- app/js/application.js
+++ app/js/application.js
@@ -1,6 +1,1 @@
-
 window.Todos = Ember.Application.create();
-// this creates a new instance of Ember.Application and 
-// makes it available as a variable named Todos within your 
-// browsers JavaScript environment.
-
--- app/js/router.js
+++ app/js/router.js
@@ -1,7 +1,3 @@
-
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
 });
-// this tells Ember.js to detect when the applications 
-// URL matches '/' and to render the todos template.
-
